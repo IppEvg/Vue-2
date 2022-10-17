43bacbade0551d2df31f5d3bc72ac55b0822bda0
Evgeniy Ippolitov <ippevg@yandex.ru>
2022-10-17 16:00:20 +0300

    Homework1

---
+++ calculator/src/App.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <h3>Simple calculator</h3>
+      <div class="window">
+        <input type="number" v-model="operand1" />
+        <div class="sign">{{sign}}</div>
+        <input type="number" v-model="operand2" />
+        <div class="sign">=</div>
+        <div class="result">{{result}}</div>
+      </div>
+      <div>
+        <button @click="sign='+'">+</button>
+        <button @click="sign='-'">-</button>
+        <button @click="sign='*'">*</button>
+        <button @click="sign='/'">/</button>
+        <button @click="sign='x^'"> x^</button>
+        <button @click="sign='DIV'">DIV</button>
+        <button @click="doOperation">=</button>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      operand1: "",
+      operand2: "",
+      sign: "+",
+      result: ""
+    }
+  },
+  methods: {
+    doOperation() {
+      if (this.sign == '+') {
+        this.result = (+this.operand1) + (+this.operand2);
+      } else if (this.sign == '-') {
+        this.result = (+this.operand1) - (+this.operand2)
+      } else if (this.sign == '*') {
+        this.result = (+this.operand1) * (+this.operand2);
+      } else if (this.sign == '/') {
+        this.result = (+this.operand1) / (+this.operand2);
+      } else if (this.sign == 'x^') {
+        if (this.operand1) {
+          this.result = (+this.operand1) * (+this.operand1);
+          this.operand2 = "";
+        } else if (this.operand2) {
+          this.result = (+this.operand2) * (+this.operand2);
+          this.operand1 = "";
+        } else {
+          this.result = 'введи операнд';
+        }
+      } else if (this.sign == 'DIV') {
+        this.result = Math.floor((+this.operand1) / (+this.operand2));
+      }
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.wrapper {
+  width: 320px;
+  height: 300px;
+  margin: 0 auto;
+  background-color: rgb(202, 206, 154);
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.window {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 10px 0px 40px;
+  border: 1px solid rgb(88, 55, 110);
+  padding: 3px;
+}
+
+input {
+  max-width: 80px;
+  font-size: 15px;
+  height: 20px;
+}
+
+.result {
+  min-width: 80px;
+  border: 2px solid rgb(88, 55, 110);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  min-width: 35px;
+  min-height: 35px;
+  margin: 3px;
+  border-radius: 5px;
+  border: 1px solid rgb(88, 55, 110);
+}
+
+button:hover {
+  box-shadow: 0px 0px 10px -2px rgb(88, 55, 110, 0.8);
+}
+
+.sign {
+  margin: 5px;
+}
+</style>
